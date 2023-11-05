d0c28c71b8e4fdb5041953e7361600b6480800b2
Viktor Filippov <jackal3166@gmail.com>
2023-11-05 19:08:53 +0200

    Create Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,25 @@
+import { links } from '../data';
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className='bg-sky-100'>
+			<div className='flex flex-col px-8 py-4 mx-auto max-w-7xl sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
+				<h2 className='text-3xl font-bold'>
+					Site<span className='text-sky-600'>Craft</span>
+				</h2>
+				<div className='flex gap-x-3'>
+					{links.map((link) => (
+						<a
+							key={link.id}
+							href={link.href}
+							className='text-xl tracking-wide capitalize duration-300 hover:text-sky-600'>
+							{link.text}
+						</a>
+					))}
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
